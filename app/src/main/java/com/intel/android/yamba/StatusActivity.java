92c7e032e5b8f23353f7f5e4f3a6e5d2e52a4ed9
Dave Smith <smith@wiresareobsolete.com>
2014-09-23 12:40:57 -0700

    Now with landscape layout and event handlers.

--- app/src/main/java/com/intel/android/yamba/StatusActivity.java
+++ app/src/main/java/com/intel/android/yamba/StatusActivity.java
@@ -1,19 +1,43 @@
 package com.intel.android.yamba;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements TextWatcher {
+
+    private static final int MAX_CHARS = 140;
+
+    private EditText mStatusText;
+    private TextView mCounterText;
+    private Button mPostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+        mStatusText = (EditText) findViewById(R.id.text_status);
+        mCounterText = (TextView) findViewById(R.id.text_counter);
+        mPostButton = (Button) findViewById(R.id.button_status);
+
+        mStatusText.addTextChangedListener(this);
+        mStatusText.setText(null);
     }
 
+    public void onPostClick(View v) {
+        Toast.makeText(this, mStatusText.getText(), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
      public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +59,26 @@ public class StatusActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+    @Override
+       public void afterTextChanged(Editable s) {
+        int remaining = MAX_CHARS - mStatusText.length();
+        mCounterText.setText(String.valueOf(remaining));
+
+        boolean disabled = (remaining < 0);
+        if (disabled) {
+            mCounterText.setTextColor(Color.RED);
+            mPostButton.setEnabled(false);
+        } else {
+            mCounterText.setTextColor(Color.BLACK);
+            mPostButton.setEnabled(true);
+        }
     }
 }
